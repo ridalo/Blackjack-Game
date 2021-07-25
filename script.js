30f50c0fcbbbab014c9a4d102aff7a00fc914ec8
ridalo <ridalo1989@gmail.com>
2021-07-25 18:53:29 -0400

    booleans

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let firstCard = 3
 let secondCard = 7
-
+let hasBlackJack = false
+let isAlive = true
 let sum = firstCard + secondCard
+
 if(sum < 21) {
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
     console.log("You got Blackjack!")
-} else if (sum > 21){
+    hasBlackJack = true
+} else {
     console.log("You have lost...")
+    isAlive = false
 }
